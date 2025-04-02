da548925729905973f05ff3e42b3ee21f7c2abc6
NPC <akuhitaminiakunam@gmail.com>
2025-04-02 23:21:25 +0700

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,8 +2,7 @@
 set -euxo pipefail
 
 # Checking MySQL TCP connection
-scripts/wait-for-it.sh --timeout=60 $DB_HOST
-#:$DB_PORT
+scripts/wait-for-it.sh --timeout=60 $DB_HOST:$DB_PORT
 
 # Checking Redis connection
 scripts/wait-for-it.sh --timeout=60 $REDIS_HOST:$REDIS_PORT
